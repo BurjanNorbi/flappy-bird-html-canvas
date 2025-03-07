27c51b28a140601acb3e29848ff8599c60db6415
Norbi <norbi010101@gmail.com>
2025-03-07 19:04:17 +0100

    drawing the bird

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,16 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+const bird = new Bird(50, canvas.height / 2, 30, 50);
+
 draw();
 
 function draw() {
+	// draw the backgound
 	ctx.fillStyle = 'blue';
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	// draw the bird
+	ctx.fillStyle = 'brown';
+	ctx.fillRect(bird.x, bird.y, bird.w, bird.h);
 }
