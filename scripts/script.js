3856066e92bcc20506ef1a3fc7b1ee7bfbcaf3e8
Norbi <norbi010101@gmail.com>
2025-03-07 18:59:45 +0100

    drawing the background

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,9 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+
+draw();
+
+function draw() {
+	ctx.fillStyle = 'blue';
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
